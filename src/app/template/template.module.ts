2c7d06f56dc4fadc98a0843193400cae201738c5
adanga89 <cuadrado015@gmail.com>
2022-06-06 13:15:46 -0500

    Creacion de Rutas lazyload

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TemplateRoutingModule } from './template-routing.module';
+import { DinamicosComponent } from './dinamicos/dinamicos.component';
+import { SwitchesComponent } from './switches/switches.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DinamicosComponent,
+    SwitchesComponent
+  ],
   imports: [
     CommonModule,
     TemplateRoutingModule
