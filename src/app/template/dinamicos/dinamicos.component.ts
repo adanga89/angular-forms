8cf762cab8614bbf433b716560e0a6eb0090da25
adanga89 <cuadrado015@gmail.com>
2022-06-07 17:22:54 -0500

    Template Dinamicos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -18,6 +18,7 @@ interface Favorito{
 })
 export class DinamicosComponent {
 
+  nuevoJuego: string = '';
   persona: Persona = {
     nombre: "Adan",
     favoritos: [
@@ -32,6 +33,13 @@ export class DinamicosComponent {
 
   eliminar(index: number){
     this.persona.favoritos.splice(index,1);
+  }
+
+  agregarJuego(){
+    const nuevoFavorito: Favorito = {id:this.persona.favoritos.length+1, 
+      nombre: this.nuevoJuego};
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego = '';
   }
 
 }
