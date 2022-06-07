6015925c2d09f7d65f8c37bd50452bee0ccb868d
adanga89 <cuadrado015@gmail.com>
2022-06-06 21:07:51 -0500

    Validaciones de Formulario

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  nombreValido(): boolean{
+    return this.miFormulario?.controls['producto'].invalid &&
+    this.miFormulario?.controls['producto'].touched;
+  }
+
+  precioValido(): boolean{
+    return this.miFormulario?.controls['precio'].touched &&
+           this.miFormulario?.controls['precio'].value < 0;
+  }
+
+  //guardarInfo( miFormulario: NgForm){
+  guardarInfo(){
+    console.log(this.miFormulario);
+  }
 }
