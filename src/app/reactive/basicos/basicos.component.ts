f1b04af79bf486d01c4129b782668f96f8162c1d
adanga89 <cuadrado015@gmail.com>
2022-06-07 18:32:19 -0500

    FormBuilder

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -9,8 +9,17 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class BasicosComponent {
 
-  miFormulario: FormGroup = new FormGroup({
-    nombre: new FormControl('RTX 4300i')
-  });
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre: new FormControl('RTX 4300i'),
+  //   precio: new FormControl(1500),
+  //   existencias: new FormControl(5)
+  // });
 
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['RTX 4080i'],
+    precio: [ 1500 ],
+    existencias: [ 5 ]
+  })
+
+  constructor(private fb: FormBuilder){}
 }
