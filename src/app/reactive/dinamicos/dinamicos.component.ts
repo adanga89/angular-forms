1abd3aec168d3c35a5894da89610a2a47ef6775e
adanga89 <cuadrado015@gmail.com>
2022-06-08 09:36:35 -0500

    FormArray | Form Reactive

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -8,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['',[Validators.required, Validators.min(3)]],
+    favoritos: this.fb.array([
+      ['Metal Gear'], ['Resident Evil 4']
+    ], Validators.required)
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  get favoritosArr(){
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
+
+  campoValido(campo: string){
+    return this.miFormulario.controls[campo].errors &&
+           this.miFormulario.controls[campo].touched;
+  }
+
+  guardar():void{
+    if(this.miFormulario.invalid){
+      console.log('nvalid')
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
   }
 
 }
