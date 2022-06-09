e04df3405a8bf691c0f3c29cf9852cc790a48c5d
adanga89 <cuadrado015@gmail.com>
2022-06-09 17:23:27 -0500

    Formulario Pendiente

--- src/app/shared/validator/email-validator.service.ts
+++ src/app/shared/validator/email-validator.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
-import { map, Observable } from 'rxjs';
+import { map, Observable, delay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +15,7 @@ export class EmailValidatorService implements AsyncValidator{
     const email = control.value;
     return this.http.get<any[]>(`http://localhost:3000/usuarios?q=${email}`)
             .pipe(
+              delay(3000),
               map( res => {
                 console.log('res:'+res);
                 return (res.length === 0) ? null : {emailTomado: true};
